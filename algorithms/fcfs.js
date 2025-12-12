a8fa92d2d0c282305b1478a9bb8b1b2b2fc8c1fa
shankar123457 <shankar007gmat@gmail.com>
2025-12-12 11:20:52 +0000

    Update FCFS algorithm logic

--- algorithms/fcfs.js
+++ algorithms/fcfs.js
@@ -1,12 +1,12 @@
-// algorithms/fcfs.js
+// // algorithms/fcfs.js
 
-export default function compute(requests, head) {
-  const sequence = [head, ...requests];
-  let totalSeek = 0;
+// export default function compute(requests, head) {
+//   const sequence = [head, ...requests];
+//   let totalSeek = 0;
 
-  for (let i = 1; i < sequence.length; i++) {
-    totalSeek += Math.abs(sequence[i] - sequence[i - 1]);
-  }
+//   for (let i = 1; i < sequence.length; i++) {
+//     totalSeek += Math.abs(sequence[i] - sequence[i - 1]);
+//   }
 
-  return { sequence, totalSeek };
-}
+//   return { sequence, totalSeek };
+// }
